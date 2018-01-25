27f4a09f170429baf43a29d477e5df32b31a7e7f
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-25 10:21:07 +0800

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -91,7 +91,7 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
-          //std::cout << ptsx[0] << std::endl;
+          std::cout << ptsx[0] << std::endl;
 
           /*
           * TODO: Calculate steering angle and throttle using MPC.
