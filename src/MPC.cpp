2751790cf04ece6b380b87cc52e17ea21f9ec291
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-29 10:28:45 +0800

    update

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -73,7 +73,6 @@ class FG_eval {
     fg[1 + cte_start] = vars[cte_start];
     fg[1 + epsi_start] = vars[epsi_start];    
     fg[1 + dir_start] = vars[dir_start];
-    cout << "checkpoint 3" << endl;
     // The rest of the constraints - future steps
     for (int t = 1; t < N; t++) {
       // The state at time t+1 .
@@ -83,8 +82,7 @@ class FG_eval {
       AD<double> v1 = vars[v_start + t];
       AD<double> cte1 = vars[cte_start + t];
       AD<double> epsi1 = vars[epsi_start + t];
-      AD<double> dir = vars[dir_start]; 
-      cout << "checkpoint 4.0."<< t << endl;     
+      AD<double> dir = vars[dir_start];     
 
       // The state at time t.
       AD<double> x0 = vars[x_start + t - 1];
@@ -92,24 +90,19 @@ class FG_eval {
       AD<double> psi0 = vars[psi_start + t - 1];
       AD<double> v0 = vars[v_start + t - 1];
       AD<double> cte0 = vars[cte_start + t - 1];
-      AD<double> epsi0 = vars[epsi_start + t - 1];
-      cout << "checkpoint 4.1."<< t << endl;     
+      AD<double> epsi0 = vars[epsi_start + t - 1];     
 
       // Only consider the actuation at time t.
       AD<double> delta0 = vars[delta_start + t - 1];
-      AD<double> a0 = vars[a_start + t - 1];
-      cout << "checkpoint 4.2."<< t << endl;     
-      
+      AD<double> a0 = vars[a_start + t - 1];         
       AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0,2) + coeffs[3] * CppAD::pow(x0,3);
-      AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0  + 3 * coeffs[3] * CppAD::pow(x0,2)) + dir * M_PI;
-      cout << "checkpoint 4.3."<< t << endl;     
+      AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0  + 3 * coeffs[3] * CppAD::pow(x0,2)) + dir * M_PI;     
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
       fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
       fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
       fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
       fg[1 + cte_start + t] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
-      fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);         
-      cout << "checkpoint 4.4."<< t << endl;     
+      fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);              
     }
     
   }
@@ -196,8 +189,6 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs, int& x_
   constraints_lowerbound[cte_start] = cte;
   constraints_lowerbound[epsi_start] = epsi;
   constraints_lowerbound[dir_start] = x_direction;
-  //constraints_lowerbound[epsi_start + N] = x_direction;
-
 
   constraints_upperbound[x_start] = x;
   constraints_upperbound[y_start] = y;
@@ -205,7 +196,6 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs, int& x_
   constraints_upperbound[v_start] = v;
   constraints_upperbound[cte_start] = cte;
   constraints_upperbound[epsi_start] = epsi;
-  //constraints_upperbound[epsi_start + N] = x_direction;
   constraints_upperbound[dir_start] = x_direction;
 
   // object that computes objective and constraints
@@ -231,12 +221,10 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs, int& x_
 
   // place to return solution
   CppAD::ipopt::solve_result<Dvector> solution;
-  cout << "check 1" << endl;
   // solve the problem
   CppAD::ipopt::solve<Dvector, FG_eval>(
       options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
       constraints_upperbound, fg_eval, solution);
-  cout << "check 2" << endl;
 
   // Check some of the solution values
   ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
