2d15c606b71c1c13d5ec02443e81973a4480cf5d
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-30 17:21:32 +0800

    update

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -8,7 +8,7 @@ using CppAD::AD;
 // TODO: Set the timestep length and duration
 //telemetry comes at the average frequency of 155 milliseconds + lag
 size_t N = 40;
-double dt = 0.05;
+double dt = 0.2;
 
 // This value assumes the model presented in the classroom is used.
 //
