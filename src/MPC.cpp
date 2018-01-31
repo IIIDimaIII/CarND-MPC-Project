f5d51eb9cb4d979feebce0eee8802f90f378aba2
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-31 13:00:01 +0800

    update

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,7 +50,7 @@ class FG_eval {
     
     //adjusting contribution of different cost components to the total
     double k_cte = 1;
-    double k_epsi = 5;
+    double k_epsi = 10;
     double k_v = 0.0001;
 
     double k_d1 = 0;
@@ -97,13 +97,13 @@ class FG_eval {
     }
     fg[0] +=  delta_der + a_der;
 
-    /*cout << "cte_error " << cte_error << endl; 
+    cout << "cte_error " << cte_error << endl; 
     cout << "psi_error " << psi_error << endl;
     cout << "vel_error " << vel_error << endl;
     cout << "delta_cum " << delta_cum << endl;
     cout << "a_cum " << a_cum << endl;
     cout << "delta_der " << delta_der << endl;    
-    cout << "a_der " << a_der << endl;    */
+    cout << "a_der " << a_der << endl; 
     
 
     // Setup Constraints
