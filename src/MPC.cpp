6e39bd3a5740034101ff6932ee5e939367386136
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-31 12:57:46 +0800

    update

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -50,7 +50,7 @@ class FG_eval {
     
     //adjusting contribution of different cost components to the total
     double k_cte = 1;
-    double k_epsi = 1;
+    double k_epsi = 5;
     double k_v = 0.0001;
 
     double k_d1 = 0;
