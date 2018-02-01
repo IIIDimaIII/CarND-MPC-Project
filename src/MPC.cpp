9ad2561247eaedd4e97834dc6a0c6ef2adb06e5b
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-02-01 10:35:19 +0800

    update

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -46,7 +46,7 @@ class FG_eval {
     
     double k_cte = 10;
     double k_epsi = 2500;
-    double k_v = 0.0002;
+    double k_v = 0.0005;
     double k_d1 = 1;
     double k_a1 = 0;    
     double k_d2 = 1000; 
